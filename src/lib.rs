2daae7956576f86c3003f19c5f32de4955b06ab9
Shunsuke Kanda <shnsk.knd@gmail.com>
2021-07-27 12:01:16 +0900

    minor

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
 //! # mih-rs
 //!
-//! Rust implementation of multi-index hashing (MIH) for neighbor searches on 64-bit codes in the Hamming space, described in the paper
+//! [![Documentation](https://docs.rs/mih-rs/badge.svg)](https://docs.rs/mih-rs)
+//! [![Crates.io](https://img.shields.io/crates/v/mih-rs.svg)](https://crates.io/crates/mih-rs)
+//! [![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/kampersanda/mih-rs/blob/master/LICENSE)
+//!
+//! Rust implementation of multi-index hashing (MIH) for neighbor searches on binary codes in the Hamming space, described in the paper
+//!
 //! > Norouzi, Punjani, and Fleet, **Fast exact search in Hamming space with multi-index hashing**, *IEEE TPAMI*, 36(6):1107– 1119, 2014.
 //!
 //! ## Features
+//!
 //! - **Two types of neighbor searches:** mih-rs provides the two search operations:
-//!     - *Range search* finds neighbor codes whose Hamming distances to a given code are within a radius.
-//!     - *Top-K search* finds the top-K codes that are closest to a given code.
+//!   - *Range search* finds neighbor codes whose Hamming distances to a given code are within a radius.
+//!   - *Top-K search* finds the top-K codes that are closest to a given code.
+//!
 //! - **Fast and memory-efficient implementation:** The data structure is built on sparse hash tables, following the original implementation.
-//! - **Parameter free:** mih-rs automatically sets an optimal parameter of MIH depending on a given database (although you can set this manually).
+//!
+//! - **Parameter free:** mih-rs automatically sets an optimal parameter of MIH depending on a given database (although you can also set this manually).
 //!
 //! ## Example
 //!
@@ -43,6 +51,12 @@
 //!     println!("{:?}", answers); // [4, 1, 6, 0]
 //! }
 //! ```
+//!
+//! ## Binary code types
+//!
+//! `mih_rs::Index` can be built from an array of type `mih_rs::CodeInt` that is a primitive integer type supporting a popcount operation via trait `mih_rs::popcnt::Popcnt`.
+//!
+//! Currently, this library defines `mih_rs::CodeInt` for `u8`, `u16`, `u32`, `u64`, and `u128`. That is, `mih_rs::Index` supports neighbor searches on these binary code types.
 pub mod codeint;
 pub mod ls;
 pub mod mih;
